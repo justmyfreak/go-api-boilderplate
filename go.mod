163e1a393880fb6febf5f8ee3855f8e1d7cd2aef
justmyfreak <justmyfreak@gmail.com>
2020-11-18 15:09:36 +0700

    Server API Boilerplate using Go and Chi

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/justmyfreak/go-rest
+
+go 1.15
+
+require (
+	github.com/go-chi/chi v1.5.0
+	github.com/go-chi/render v1.0.1
+	github.com/joho/godotenv v1.3.0 // indirect
+)
---
+++ handler/handler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+)
+
+func NewHandler() http.Handler {
+	router := chi.NewRouter()
+
+	router.MethodNotAllowed(methodNotAllowedHandler)
+	router.NotFound(notFoundHandler)
+	
+	return router
+}
+
+func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(405)
+	render.Render(w, r, ErrMethodNotAllowed)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(400)
+	render.Render(w, r, ErrNotFound)
+}
